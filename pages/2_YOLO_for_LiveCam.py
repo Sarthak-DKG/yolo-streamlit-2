be27792bc97171b3bcd70e976e005a6aed6c0569
Sarthak-DKG <sarthak@dkgrouplabs.com>
2023-10-04 16:01:10 +0530

    Update 2_YOLO_for_LiveCam.py

--- pages/2_YOLO_for_LiveCam.py
+++ pages/2_YOLO_for_LiveCam.py
@@ -1,11 +1,9 @@
 # ------------------------------------------------------------------------------
-#  <copyright file="Home.py" company="DKGLabs Pvt Ltd">
+#  <copyright file="YOLO_for_LiveCam.py" company="DKGLabs Pvt Ltd">
 #      Copyright (c) DKGLabs Pvt Ltd. All Rights Reserved.
 #      Information Contained Herein is Proprietary and Confidential.
 #  </copyright>
 #  ------------------------------------------------------------------------------
-
-
 
 import streamlit as st 
 
