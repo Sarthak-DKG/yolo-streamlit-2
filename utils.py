806c2fb54fa0544929e36ffa6d98978b611ac213
Sarthak-DKG <sarthak@dkgrouplabs.com>
2023-10-04 16:01:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,9 @@
 # ------------------------------------------------------------------------------
-#  <copyright file="Home.py" company="DKGLabs Pvt Ltd">
+#  <copyright file="utils.py" company="DKGLabs Pvt Ltd">
 #      Copyright (c) DKGLabs Pvt Ltd. All Rights Reserved.
 #      Information Contained Herein is Proprietary and Confidential.
 #  </copyright>
 #  ------------------------------------------------------------------------------
-
-
 
 import yaml
 import os
