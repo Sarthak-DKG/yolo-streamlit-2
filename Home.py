2efdb904d8d61d9bf796d8fc76d8327a02200e0b
Sarthak-DKG <sarthak@dkgrouplabs.com>
2024-01-22 21:23:49 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@
 #  </copyright>
 #  ------------------------------------------------------------------------------
 
-import streamlit as st 
+import streamlit as st
 import yaml
 from utils import Utils
 from utils import logo
@@ -13,12 +13,10 @@ from utils import logo
 print("PageName")
 print(__name__)
 
-st.set_page_config(page_title="Home",
-                   layout='wide',
-                   page_icon='./images/home.png')   
+st.set_page_config(page_title="Home", layout="wide", page_icon="./images/home.png")
 
 st.title("YOLO Object Detection App")
-st.caption('This web application demostrate Object Detection')
+st.caption("This web application demostrate Object Detection")
 st.header("Which model would you like to use?")
 logo()
 folderList, fileDict = Utils.load_model_data("./models")
@@ -26,27 +24,42 @@ Utils.set_model("")
 Utils.set_yaml("")
 
 print("@".join(folderList))
-model_selected = st.selectbox("Which model would you like to use?"  ,folderList , index=None, placeholder="Choose your model")
+model_selected = st.selectbox(
+    "Which model would you like to use?",
+    folderList,
+    index=None,
+    placeholder="Choose your model",
+)
 
 print(model_selected)
 if model_selected != None:
-    Utils.set_model(model_selected) 
+    Utils.set_model(model_selected)
     Utils.set_yaml(model_selected)
     if model_selected[-2:] == "v5":
-        st.markdown("""
+        st.markdown(
+            """
             # - [Image Detection](/YOLO_for_image/)
             # - [Live Camera Detection](/YOLO_for_LiveCam/)   
-                        """)
+                        """
+        )
         st.write("This model helps in detecting using Yolo V5")
     else:
-        st.markdown("""
+        st.markdown(
+            """
             # - [Image Detection](/YOLOv8_for_image/)
             # - [Live Camera Detection](/YOLOv8_for_LiveCam/) 
             # - [Video Detection](/YOLOv8_for_Video/)
-                        """)  
+                        """
+        )
         st.write("This model helps in detecting using Yolo V8")
 
-
-    with open(Utils.get_yaml(), 'r') as file1:
+    with open(Utils.get_yaml(), "r") as file1:
         yaml_file1_data = yaml.safe_load(file1)
-        st.write(yaml_file1_data['names'])
+        Utils.set_yaml_data_list(yaml_file1_data["names"])
+        classList = st.multiselect(
+            "Choose the objects for detection", options=yaml_file1_data["names"]
+        )
+
+    st.write("You selected : ")
+    st.write(classList)
+    Utils.set_object_list(classList)
