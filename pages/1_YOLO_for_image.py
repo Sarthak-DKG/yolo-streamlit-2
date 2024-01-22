9384f9c3fee3dc8e3edbe9aadf00fdd0ff2aa050
Sarthak-DKG <sarthak@dkgrouplabs.com>
2024-01-22 21:23:53 +0530

    Update 1_YOLO_for_image.py

--- pages/1_YOLO_for_image.py
+++ pages/1_YOLO_for_image.py
@@ -12,73 +12,77 @@ import numpy as np
 from utils import Utils
 from utils import logo
 
-st.set_page_config(page_title="YOLO Object Detection",
-                   layout='wide',
-                   page_icon='./images/object.png')
+st.set_page_config(
+    page_title="YOLO Object Detection", layout="wide", page_icon="./images/object.png"
+)
 
-st.header("Get Object Detection on Image using YOLOv5 Model: {modelName}".format(modelName = Utils.get_model_name()))
-st.write('Please Upload an Image to get the detections')
+# st.header("Get Object Detection on Image using YOLOv5 Model: {modelName}".format(modelName = Utils.get_model_name()))
+st.header("Get Object Detection on Image")
+st.write("Please Upload an Image to get the detections")
 logo()
 
-with st.spinner('Please wait while your model is loading'):
-    yolo = YOLO_Pred(onnx_model=Utils.get_model(),
-                    data_yaml=Utils.get_yaml())
-    #st.balloons()
+with st.spinner("Please wait while your model is loading"):
+    yolo = YOLO_Pred(onnx_model=Utils.get_model(), data_yaml=Utils.get_yaml())
+    # st.balloons()
+
 
 def upload_image():
     # Upload Image
-    image_file = st.file_uploader(label='Upload Image')
+    image_file = st.file_uploader(label="Upload Image")
     if image_file is not None:
-        size_mb = image_file.size/(1024**2)
-        file_details = {"filename":image_file.name,
-                        "filetype":image_file.type,
-                        "filesize": "{:,.2f} MB".format(size_mb)}
-        #st.json(file_details)
+        size_mb = image_file.size / (1024**2)
+        file_details = {
+            "filename": image_file.name,
+            "filetype": image_file.type,
+            "filesize": "{:,.2f} MB".format(size_mb),
+        }
+        # st.json(file_details)
         # validate file
-        if file_details['filetype'] in ('image/png','image/jpeg'):
-            st.success('VALID IMAGE file type (png or jpeg')
-            return {"file":image_file,
-                    "details":file_details}
-        
+        if file_details["filetype"] in ("image/png", "image/jpeg"):
+            st.success("VALID IMAGE file type (png or jpeg")
+            return {"file": image_file, "details": file_details}
+
         else:
-            st.error('INVALID Image file type')
-            st.error('Upload only png,jpg, jpeg')
+            st.error("INVALID Image file type")
+            st.error("Upload only png,jpg, jpeg")
             return None
-        
+
+
 def main():
     object = upload_image()
-    
+
     if object:
         prediction = False
-        image_obj = Image.open(object['file'])       
-        
-        col1 , col2 = st.columns(2)
-        
+        image_obj = Image.open(object["file"])
+
+        col1, col2 = st.columns(2)
+
         with col1:
-            st.info('Preview of Image')
+            st.info("Preview of Image")
             st.image(image_obj)
-            
-        button = st.button('Get Detection from YOLO')
+
+        button = st.button("Get Detection")
         if button:
-                with st.spinner("""
+            with st.spinner(
+                """
                 Geting Objets from image. please wait
-                                """):
-                    # below command will convert
-                    # obj to array
-                    image_array = np.array(image_obj)
-                    pred_img = yolo.predictions(image_array)
-                    pred_img_obj = Image.fromarray(pred_img)
-                    prediction = True
-        with col2:        
+                                """
+            ):
+                # below command will convert
+                # obj to array
+                image_array = np.array(image_obj)
+                pred_img = yolo.predictions(image_array)
+                pred_img_obj = Image.fromarray(pred_img)
+                prediction = True
+        with col2:
             if prediction:
                 st.subheader("Predicted Image")
-                st.caption("Object detection from YOLO V5 model")
+                st.caption("Object detection")
                 st.image(pred_img_obj)
-    
-        st.subheader('Check below for file details')
-        st.json(object['details'])
-       
-    
-    
+
+        st.subheader("Check below for file details")
+        st.json(object["details"])
+
+
 if __name__ == "__main__":
     main()
